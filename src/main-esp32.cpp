f06168852ba62ed0a426ba184ff24894159dde8d
Patraxx <patrik.cederlund@gmail.com>
2025-03-21 12:03:45 +0100

    ASD

--- src/main-esp32.cpp
+++ src/main-esp32.cpp
@@ -3,12 +3,14 @@
 
 
 #define OUTPUT_PIN 32
+#define MULTIPLEXER_1_DISABLE 17
+#define MULTIPLEXER_2_DISABLE 5
 
 #define MULTIPLEX_A 26
 #define MULTIPLEX_B 25
 #define MULTIPLEX_C 33
 
-#define CYCLE_PIN_BUTTON 31
+#define CYCLE_PIN_BUTTON 13
 
 int currentPinSelected = 0;
 
@@ -24,49 +26,63 @@ int selectionArray[8][3] = {
   {1, 1, 1}
 };
 
-void cyclePinButtonTask(void *pvParameters) {
 
-  while(1)  {
+void multiplex_looper_cycle_task(void *pvParameters) {
+  Serial.println("Multiplex task started");
 
-
-    if(digitalRead(CYCLE_PIN_BUTTON) == HIGH) {
-      currentPinSelected++;
-      if(currentPinSelected > 7) {
-        currentPinSelected = 0;
-      }
-      Serial.println("Current Pin Selected: " + String(currentPinSelected));
+    while (1) {
+      for (int i = 0; i < 8; i++) {
+        digitalWrite(MULTIPLEX_A, selectionArray[i][0]);
+        digitalWrite(MULTIPLEX_B, selectionArray[i][1]);
+        digitalWrite(MULTIPLEX_C, selectionArray[i][2]);  
     }
-    vTaskDelay(300/ portTICK_PERIOD_MS);
- 
-  }  
+    vTaskDelay(10/ portTICK_PERIOD_MS);   
+  }
+  vTaskDelete(NULL);
+}
 
 
+void debugPrinter(int i, int selectionArray[8][3]){
 
-
-
+    Serial.print("Selection: ");
+    Serial.print(" ");
+    Serial.print(selectionArray[i][0]);
+    Serial.print(" ");
+    Serial.print(selectionArray[i][1]);
+    Serial.print(" ");
+    Serial.println(selectionArray[i][2]);
+   
 }
 void multiplex_selection_task(void *pvParameters) {
   Serial.println("Multiplex task started");
 
-    while (1) {
+        digitalWrite(MULTIPLEX_A, selectionArray[currentPinSelected][0]);
+        digitalWrite(MULTIPLEX_B, selectionArray[currentPinSelected][1]);
+        digitalWrite(MULTIPLEX_C, selectionArray[currentPinSelected][2]);
 
-      for (int i = 0; i < 8; i++) {
-        digitalWrite(MULTIPLEX_A, selectionArray[i][0]);
-        digitalWrite(MULTIPLEX_B, selectionArray[i][1]);
-        digitalWrite(MULTIPLEX_C, selectionArray[i][2]);
+  while (1) {
 
-        Serial.print("Current Button is: " + String(i));
-        Serial.println(" || Status " + String(digitalRead(OUTPUT_PIN)));
 
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-        // Read from the OUTPUT_PI
-    }
+    if (digitalRead(CYCLE_PIN_BUTTON) == HIGH) 
+      // Debounce the button press
     
+      if (digitalRead(CYCLE_PIN_BUTTON) == HIGH) {
+        currentPinSelected++;
+        if (currentPinSelected > 7) {
+          currentPinSelected = 0;
+        }
+        vTaskDelay(150 / portTICK_PERIOD_MS);
+  
+        // Set the multiplex pins according to the current selection
+        digitalWrite(MULTIPLEX_A, selectionArray[currentPinSelected][0]);
+        digitalWrite(MULTIPLEX_B, selectionArray[currentPinSelected][1]);
+        digitalWrite(MULTIPLEX_C, selectionArray[currentPinSelected][2]);
 
-
-  }
+        debugPrinter(currentPinSelected, selectionArray);
+      }
+      vTaskDelay(5/ portTICK_PERIOD_MS);
+    }
   vTaskDelete(NULL);
-
 }
 
 void testPins(){
@@ -79,11 +95,14 @@ void testPins(){
 void setup() {
 
   pinMode(OUTPUT_PIN, INPUT_PULLDOWN);
+  pinMode(MULTIPLEXER_1_DISABLE, OUTPUT);
+  pinMode(MULTIPLEXER_2_DISABLE, OUTPUT);
   pinMode(CYCLE_PIN_BUTTON, INPUT_PULLDOWN);
   pinMode(MULTIPLEX_A, OUTPUT);
   pinMode(MULTIPLEX_B, OUTPUT);
   pinMode(MULTIPLEX_C, OUTPUT);
   Serial.begin(9600);
+  delay(500);
 
    xTaskCreate(multiplex_selection_task, "multiplex_selection_task", 2048, NULL, 1, NULL);
 
