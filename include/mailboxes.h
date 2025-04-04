d0116a3ac1f10f815b5e4b5ec3f0e88c9235be2e
Patraxx <patrik.cederlund@gmail.com>
2025-04-04 09:33:10 +0200

    plop

---
+++ include/mailboxes.h
@@ -0,0 +1,22 @@
+#ifndef MAILBOXES_H
+#define MAILBOXES_H
+#include <Arduino.h>
+#include "main.h"
+
+
+class Mailbox{
+private:
+     int sensorArray[3];
+     int mailboxNumber;
+     bool mailDetected;
+
+public:
+    Mailbox
+
+
+
+}
+
+
+
+#endif
