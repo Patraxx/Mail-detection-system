df3ad79bf1ef2a4b7449521cbce446319411a6e0
Patraxx <patrik.cederlund@gmail.com>
2025-04-07 10:21:48 +0200

    p0ko

--- include/compartments.h
+++ include/compartments.h
@@ -6,25 +6,43 @@
 
 class Compartment{
 private:
-     int sensorArray[3];
-     int CompartmentNumber;
-     String CompartmentName;
-     bool mailDetected;
+     int sensorArray[3] = {0, 0, 0};
+     int assigned_multiplexer = 0;
+     int compartmentNumber = 0;
+     String compartmentName = "None";
+     bool mailDetected = false;
 
 public:
- Compartment(int CompartmentNumber){
-        this-> CompartmentNumber = CompartmentNumber;
-        this->mailDetected = false;
-    }
+
+ Compartment(){} //default constructor
+
     //skapa en constructor där 8 st fack skapas i taget, initialiseras med multiplexer_array
  Compartment(int CompartmentNumber, int sensorArray[3]){
-        this-> CompartmentNumber = CompartmentNumber;
+        this-> compartmentNumber = CompartmentNumber;
         this->sensorArray[0] = sensorArray[0];
         this->sensorArray[1] = sensorArray[1];
         this->sensorArray[2] = sensorArray[2];
         this->mailDetected = false;
     }
+    void setCompartmentName(String name){
+        this->compartmentName = name;
+    }
+    String getCompartmentName(){
+        return this->compartmentName;
+    }
+    void setMailDetected(bool detected){
+        this->mailDetected = detected;
+    }
+    bool getMailDetected(){
+        return this->mailDetected;
+    }
+    void setAssignedMultiplexer(int multiplexer){
+        this->assigned_multiplexer = multiplexer;
+    }
+    int getAssignedMultiplexer(){
+        return this->assigned_multiplexer;
+    }
 
-    Compartment CompartmentFiller();
+    static void compartmentCreator(Compartment compartments[], int amount);
 };
 #endif
--- src/compartments.cpp
+++ src/compartments.cpp
@@ -1,4 +1,14 @@
 #include "compartments.h"
 
 
-Compartment test(1, multiplexer_array[0]);
+static void compartmentCreator(Compartment compartments[], int amount){
+
+    
+    for (int i = 0; i < amount; i++){
+        compartments[i] = Compartment(i, multiplexer_array[i]);
+        if(amount < 8){
+            compartments[i].assigned_multiplexer = 1;         
+        }
+        
+    }
+}
