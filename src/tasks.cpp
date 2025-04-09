13dc85e952be91153188477c2c6cb8bdb3bbeccd
Patraxx <patrik.cederlund@gmail.com>
2025-04-09 13:47:27 +0200

    paus

--- src/tasks.cpp
+++ src/tasks.cpp
@@ -2,36 +2,6 @@
 #include "main.h"
 #include "compartments.h"
 
-
-void multiplex_looper_task(void *pvParameters) {
-
-  CompartmentManager* compartmentManager = static_cast<CompartmentManager*>(pvParameters);
-
-  digitalWrite(MULTIPLEXER_1_DISABLE, LOW);
-  digitalWrite(MULTIPLEXER_2_DISABLE, HIGH);
-  Serial.println("Multiplex task started");
-
-    while (1) {
-      for (int i = 0; i < 8; i++) {  
-
-        Compartment* currentCompartment = &compartmentManager->compartments[i];
-              
-
-          digitalWrite(MULTIPLEX_A, multiplexer_array[i][0]);
-          digitalWrite(MULTIPLEX_B, multiplexer_array[i][1]);
-          digitalWrite(MULTIPLEX_C, multiplexer_array[i][2]);
-          vTaskDelay(10 / portTICK_PERIOD_MS);
-
-          if (digitalRead(FINAL_INPUT) == HIGH) {   
-            
-          }
-    }
-
-    vTaskDelay(10/ portTICK_PERIOD_MS);   
-  }
-  vTaskDelete(NULL);
-}
-
 void scan_multiplexer(int activeMultiplexer, const int disable_mux[], int num_mux){
 
   for (int i = 0; i < num_mux; i++) {
@@ -45,4 +15,35 @@ void scan_multiplexer(int activeMultiplexer, const int disable_mux[], int num_mu
   }
 
 }
+
+
+void multiplex_looper_task(void *pvParameters) {
+
+  CompartmentManager* compartmentManager = static_cast<CompartmentManager*>(pvParameters);
+
+  Serial.println("Multiplex task started");
+
+    while (1) {
+
+      for (int i = 0; i < NUMBER_OF_COMPARTMENTS; i++) {
+
+        
+       
+      }
+
+
+      scan_multiplexer(0, multiplexer_disable_pins, 2); // Scan the first multiplexer
+      scan_multiplexer(1, multiplexer_disable_pins, 2); // Scan the second multiplexer
+
+       // Compartment* currentCompartment = &compartmentManager->compartments[i];
+            
+    }
+
+  vTaskDelete(NULL);
+
+}
+
+
+
+
 
