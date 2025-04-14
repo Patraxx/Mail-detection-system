42a2e55184552ad6a61ba475bac2fd48646bcaad
patraxx <patrik.cederlund@gmail.com>
2025-04-14 11:01:43 +0200

    hardwareSeroaö

--- src/tasks.cpp
+++ src/tasks.cpp
@@ -69,15 +69,16 @@ void mailbox_printer_task(void *pvParameters) {
   }
 }
 
-void sendMailboxStatusCSV(CompartmentManager* compartmentManager) {
+void sendMailboxStatusCSV(HardwareSerial Serial, CompartmentManager* compartmentManager) {
   int totalCompartments = compartmentManager->totalCompartments; // Get the total number of compartments
   if (xSemaphoreTake(compartmentMutex, portMAX_DELAY)) {
     for (int i = 0; i < totalCompartments; i++) {
-      Serial2.print(compartmentManager->compartments[i].mailDetected ? "1" : "0"); // Send 1 if mail is detected, otherwise send 0
+      Serial.print(compartmentManager->compartments[i].mailDetected ? "1" : "0"); // Send 1 if mail is detected, otherwise send 0
       if (i < totalCompartments - 1) {
-        Serial2.print(","); // Add comma between values
+        Serial.print(","); // Add comma between values
       }
     }
+    Serial.print("\n"); // Send newline after all values
     xSemaphoreGive(compartmentMutex); // Release the mutex after sending the header
   }
 }
